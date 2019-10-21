1dc9de6f9848bdd0633f545180f3a3cd93c733a3
Benson <bensontruong@ucsb.edu>
2019-10-21 13:57:11 -0700

    add infant mortality graph

--- part_3_bt.R
+++ part_3_bt.R
@@ -6,8 +6,17 @@ library(tidyverse)
 library(janitor)
 library(here)
 
-###########
 # Read in data:
 db <- read_csv(here("data", "disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100K = death_rate_per_100_000)
+
+# Create a subset, then graph:
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia"),
+         age_group == "0-6 days",
+         sex == "Both")
+  
+# Create a basic line graph:
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100K))+
+  geom_line(aes(color = country_name))
